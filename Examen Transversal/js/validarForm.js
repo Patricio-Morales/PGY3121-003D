3bea7c246e9d17c638e94f61ffad50ae36533e83
Patricio-Morales <pat.moralesv@duocuc.cl>
2023-05-12 17:47:47 -0400

    Formularios validados

---
+++ Examen Transversal/js/validarForm.js
@@ -0,0 +1,47 @@
+document.getElementById("valSku").style.display="none";
+
+document.getElementById("valProd").style.display="none";
+
+document.getElementById("valCant").style.display="none";
+
+
+
+function validarAgregarSku() {
+	if (document.getElementById("txtSku").value.length==0) {
+		document.getElementById("valSku").style.display="inline";
+        document.getElementById("txtSku").classList.add("is-invalid")
+		
+	} else {
+		document.getElementById("valSku").style.display="none";
+        document.getElementById("txtSku").classList.remove("is-invalid")
+		document.getElementById("txtSku").classList.add("is-valid")
+	}
+	
+}
+
+
+
+function validarAgregarNomProd() {
+	if (document.getElementById("txtProd").value.length==0) {
+		document.getElementById("valProd").style.display="inline";
+        document.getElementById("txtProd").classList.add("is-invalid")
+		
+	 } else {
+		document.getElementById("valProd").style.display="none";
+        document.getElementById("txtProd").classList.remove("is-invalid")
+		document.getElementById("txtSku").classList.add("is-valid")
+	 }
+}
+
+function validarCantidad() {
+	if (document.getElementById("txtCant").value.length==0) {
+		document.getElementById("valCant").style.display="inline";
+        document.getElementById("txtCant").classList.add("is-invalid")
+		
+	 } else {
+		document.getElementById("valCant").style.display="none";
+        document.getElementById("txtCant").classList.remove("is-invalid")
+		document.getElementById("txtCant").classList.add("is-valid")
+	 }
+	
+}
